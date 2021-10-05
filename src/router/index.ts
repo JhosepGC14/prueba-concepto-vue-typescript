1bb99e3eca0a1a79de93b85a65c811cf244c85b4
Jhosep Guadalupe <jhosep.guadalupe>
2021-10-04 23:04:43 -0500

    prueba de concepto con vue y typescript

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,14 +13,12 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import("../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/services",
     name: "Services",
-    component: () =>
-      import("../views/Services.vue"),
+    component: () => import("../views/Services.vue"),
   },
 ];
 
